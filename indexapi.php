e5d6c632700562972ae2f93366cbd649c050217e
GabrielTHirai <gabrielt.hirai@gmail.com>
2022-11-29 14:10:32 -0300

    terminando o backend

---
+++ indexapi.php
@@ -0,0 +1,42 @@
+<?php
+$formulario = file_get_contents("index.html");
+
+if (isset($_REQUEST['bPesquisar']))
+{
+    $cep = $_REQUEST['cep'];
+
+    $curl   = curl_init();
+    curl_setopt_array( $curl, [
+CURLOPT_RETURNTRANSFER => 1,
+CURLOPT_URL => "https://viacep.com.br/ws/$cep/json/"
+         ]
+    );
+
+    $resposta = curl_exec($curl);
+    curl_close($curl);
+    
+    // transformar a resposta em um array
+    $dados    = json_decode($resposta, true);
+    if (isset($dados["logradouro"])){
+    $endereco = "<h2>Logradouro: " . $dados["logradouro"];
+    $endereco = $endereco . "<br>" .
+                "Bairro: "     . $dados["bairro"];
+    $endereco = $endereco . "<br>" .
+                "Cidade: "     . $dados["localidade"];
+    $endereco = $endereco . "<br>" .
+                "Estado: "     . $dados["uf"] . "</h2>";
+    }
+    else{
+       $endereco = "<h2>Cep inválido ou inexistente.</h2>"; 
+    }
+
+    $formulario = str_replace("<!--ENDERECO-->", 
+                               $endereco, 
+                               $formulario);
+
+}
+
+echo $formulario;
+
+
+?>
